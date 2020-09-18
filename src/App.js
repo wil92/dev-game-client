0ca57a0cb2cf7380a341a879febf4358f68a1dc1
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-18 02:10:04 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = "ws://localhost:3002";
+
+class App extends React.Component {
+    ws = new WebSocket(URL);
+
+    rowSize = 8;
+
+    players = [];
+    field = [];
+
+    componentDidMount() {
+        this.ws.onopen = () => {
+            console.log('connected');
+        }
+
+        this.ws.onmessage = evt => {
+            try {
+                this.players = JSON.parse(evt.data);
+                this.paintPlayers();
+            } catch (er) {
+                console.log(er);
+            }
+        }
+
+        fetch('http://localhost:3000/game/field')
+            .then(res => res.json())
+            .then((data) => {
+                if (data) {
+                    this.setState({
+                        height: data.length * this.rowSize,
+                        width: data[0].length * this.rowSize
+                    });
+                    this.field = data;
+                }
+            })
+            .catch(console.log)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.context = this.refs.canvas.getContext('2d');
+        this.cleanField();
+    }
+
+    paintPlayers() {
+        this.cleanField();
+        this.players.forEach(player => {
+            this.context.fillStyle = player.color;
+            const xpos = player.position.x * this.rowSize, ypos = player.position.y * this.rowSize;
+            // this.context.fillRect(xpos, ypos, this.rowSize, this.rowSize);
+            const radius = this.rowSize / 2;
+            this.context.beginPath();
+            this.context.arc(xpos + radius, ypos + radius, radius, 0, 2 * Math.PI);
+            this.context.fill();
+        });
+    }
+
+    cleanField() {
+        for (let i = 0; i < this.field.length; i++) {
+            for (let j = 0; j < this.field[i].length; j++) {
+                this.context.fillStyle = this.field[i][j] ? '#ffffff' : '#000000';
+                this.context.fillRect(i * this.rowSize, j * this.rowSize, this.rowSize, this.rowSize);
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <canvas ref="canvas"
+                            height={this.state?.height}
+                            width={this.state?.width}/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
