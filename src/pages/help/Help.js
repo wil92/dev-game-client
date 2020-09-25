208b50b3966fdb3c1bb646f84e2678224b825730
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-25 13:32:09 +0200

    Show markdown help in the help component

--- src/pages/help/Help.js
+++ src/pages/help/Help.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import ReactMarkdown from "react-markdown";
 
 import './Help.css';
 import '../../common.css';
 import Toolbar from "../../components/toolbar/Toolbar";
+import {getApiUrl} from "../../utils/urls";
 
 class Help extends React.Component {
+
+    componentDidMount() {
+        this.url = getApiUrl();
+
+        this.fetchHelpText();
+    }
+
+    fetchHelpText() {
+        fetch(`/help-en.md`)
+            .then(help => help.text())
+            .then((help) => {
+                console.log(help);
+                if (help) {
+                    this.setState({help})
+                }
+            })
+            .catch(console.log)
+    }
+
     render() {
         return (
             <div className="Page">
                 <Toolbar history={this.props?.history}/>
-                <div className="PageContent">
-                    help
+                <div className="PageContent HelpPageContent">
+                    <ReactMarkdown source={this.state?.help || ''}/>
                 </div>
             </div>
         );
