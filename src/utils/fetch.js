632622aaa59d02cc53c9fe1f69509434f0108691
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-26 17:43:07 +0200

    Remove local token if the token is not valid

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,10 @@
+import {clearSession} from "./user-data";
+
 export function fetchInternal(url, options) {
     return fetch(url, options)
         .then(res => {
             if (res.status === 401) {
+                clearSession();
                 return window.location.href = window.location.origin;
             }
             return res;
