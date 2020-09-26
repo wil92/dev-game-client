a65addb9f60dcc11ba09e6d77096c74a3c2f964e
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-26 17:41:57 +0200

    Redirect to root if the token is not valid

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,3 +1,9 @@
 export function fetchInternal(url, options) {
-    return fetch(url, options);
+    return fetch(url, options)
+        .then(res => {
+            if (res.status === 401) {
+                return window.location.href = window.location.origin;
+            }
+            return res;
+        });
 }
